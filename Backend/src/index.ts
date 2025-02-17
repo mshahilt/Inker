1e28d99f4613fc1dde7b72b60f4abbdb59c512d2
Shammasahamedp <shammasahamedp123@gmail.com>
2025-02-17 18:53:56 +0530

    feat: implemented jwt functions and auth router

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import { Request, Response } from "express"
 import { connectDb } from "./configs/mongo.config"
 import { connectRedis } from "./configs/redis.config"
+import authRouter from "./routers/auth.router"
 const PORT = 3000
 const app = express()
 app.use(cors())
@@ -20,6 +21,8 @@ app.get('/',(req:Request,res:Response)=>{
 
     res.status(200).json({message:"success"})
     return 
-})  
+});
+
+app.use('/auth', authRouter)
 
 app.listen(PORT, () => console.log(`Server started at ${PORT} ✅`))
