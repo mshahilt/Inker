9a54366108179fd731ab8f37a1c0ed2ad15d1f4c
shreedev44 <shreedevchambayil@gmail.com>
2025-02-19 15:18:17 +0530

    fix: set limit for redis connection retries

--- Backend/src/utils/validate-env.util.ts
+++ Backend/src/utils/validate-env.util.ts
@@ -1,19 +1,22 @@
 import { env } from "../configs/env.config";
 
 export function validateEnv() {
-    if(!env.PORT) {
-        throw new Error("PORT is not found in the env")
-    }
-    if(!env.MONGO_URI) {
-        throw new Error("MONGO_URI is not found in the env")
-    }
-    if(!env.REDIS_URI) {
-        throw new Error("REDIS_URI is not found in the env")
-    }
-    if(!env.JWT_ACCESS_SECRET) {
-        throw new Error("JWT_ACCESS_SECRET is not found in the env")
-    }
-    if(!env.JWT_REFRESH_SECRET) {
-        throw new Error("JWT_REFRESH_SECRET is not found in the env")
-    }
+  if (!env.PORT) {
+    throw new Error("PORT is not found in the env");
+  }
+  if (!env.MONGO_URI) {
+    throw new Error("MONGO_URI is not found in the env");
+  }
+  if (!env.REDIS_HOST) {
+    throw new Error("REDIS_HOST is not found in the env");
+  }
+  if (!env.REDIS_PORT) {
+    throw new Error("REDIS_PORT is not found in the env");
+  }
+  if (!env.JWT_ACCESS_SECRET) {
+    throw new Error("JWT_ACCESS_SECRET is not found in the env");
+  }
+  if (!env.JWT_REFRESH_SECRET) {
+    throw new Error("JWT_REFRESH_SECRET is not found in the env");
+  }
 }
