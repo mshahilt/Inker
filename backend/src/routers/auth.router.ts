73c70711209e8e10d1548120d3328a54464df837
MUHAMMED HASHIM <muhammedhashim6789@gmail.com>
2025-02-21 21:19:11 +0530

    feat: created verifyOtp method in auth controller and auth service

--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -3,14 +3,14 @@ import { AuthController } from "../controllers/implementation/auth.controller";
 import { AuthService } from "../services/implementation/auth.service";
 import { UserRepository } from "../repositories/implementation/user.repository";
 
-
 const authRouter = Router();
 
 const userRepository = new UserRepository();
 const authService = new AuthService(userRepository);
 const authController = new AuthController(authService);
 
-authRouter.post('/register',authController.signup.bind(authController));
-authRouter.post('/login',authController.signin.bind(authController));
+authRouter.post("/register", authController.signup.bind(authController));
+authRouter.post("/login", authController.signin.bind(authController));
+authRouter.post("/verify", authController.verifyOtp.bind(authController));
 
 export default authRouter;
