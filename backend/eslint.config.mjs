ddcae7d13818e8cfd2ac65eef5590a7712fc9e2e
Rahil <rahilsardar234@gmail.com>
2025-02-22 15:37:12 +0530

    changes:-     - workflow lint.yaml increased warning limit to 10     - fixed type error in error.middlware.ts     - edited eslint in frontend and backend

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -5,8 +5,13 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    {files: ["**/*.{js,mjs,cjs,ts}"]},
+    pluginJs.configs.recommended,
+    {languageOptions: {globals: globals.browser}},
+    ...tseslint.configs.recommended,
+    {
+        "rules": {
+            "@typescript-eslint/no-unused-vars": ["warn"],
+        }
+    }
 ];
