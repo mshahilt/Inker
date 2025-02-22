fb8a81fe0d8ec0732dc562f35a8bee550d521492
AJMALAJU3 <ajmal.aju.340711@gmail.com>
2025-02-22 13:06:46 +0530

    fix: checked and fixed lint errors and added  package-lock to gitignore

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router-dom";
 import { SidebarTrigger } from "../ui/sidebar";
 import ThemeToggle from "./ThemeToggle";
 import SearchBar from "./SearchBar";
 
 
 const Navbar = () => {
-  const navigate = useNavigate()
+
   return (
     <header className="bg-white shadow-sm px-6 py-4 w-full ">
       <div className=" flex justify-between items-center">
@@ -14,7 +12,7 @@ const Navbar = () => {
         <div className="flex items-center gap-4">
           <SearchBar />
           <ThemeToggle />
-          <img className="w-10 h-10 rounded-md border-2" src="https://res.cloudinary.com/dwyxogyrk/image/upload/v1737433466/h0xf7zi0blmclfqrjeo7.png" alt="" />
+          <img className="w-1 h-10 rounded-md border-2" src="https://res.cloudinary.com/dwyxogyrk/image/upload/v1737433466/h0xf7zi0blmclfqrjeo7.png" alt="" />
         </div>
       </div>
     </header>
