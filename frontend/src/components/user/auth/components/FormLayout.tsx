431f3be1dc8c33292f92e5eb5b8ca1c9848ef456
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2025-02-24 15:04:55 +0530

    feat(auth): rename signin state to authState for clarity in FormLayout component

--- frontend/src/components/user/auth/components/FormLayout.tsx
+++ frontend/src/components/user/auth/components/FormLayout.tsx
@@ -7,20 +7,20 @@ import { useState } from "react"
 
 export const FormLayout = () => {
 
-    const [signin, setSignin] = useState<"register" | "login">("login");
+    const [authState, setAuthState] = useState<'login' | 'register'>('login');
 
     return (
         <div className='lg:p-8'>
             <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[350px]'>
                 <div className='flex flex-col space-y-2 text-left'>
-                    <h1 className='text-2xl font-semibold tracking-tight'>{signin === 'login' ? "Welcome Back!" : "Create an Account"}</h1>
+                    <h1 className='text-2xl font-semibold tracking-tight'>{authState === 'login' ? "Welcome Back!" : "Create an Account"}</h1>
                     <p className='text-sm text-muted-foreground'>
-                        {signin === 'login' ? "Please enter your email and password to log in." : "Please fill in the details to create your account."}
+                        {authState === 'login' ? "Please enter your email and password to log in." : "Please fill in the details to create your account."}
                     </p>
                 </div>
                 <UserAuthForm
-                    authState={signin}
-                    onStateChange={setSignin}
+                    authState={authState}
+                    onStateChange={setAuthState}
                 />
                 <div className="relative">
                     <div className="absolute inset-0 flex items-center">
