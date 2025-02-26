ccda081f739d33f84f333b60711146ca84816ffb
faizal <faisalt661@gmail.com>
2025-02-26 17:53:24 +0530

    feat: list blogs in profile feed

---
+++ frontend/src/components/user/common/BlogCard.tsx
@@ -0,0 +1,53 @@
+import { FC } from "react";
+import { EllipsisVertical } from "lucide-react";
+
+interface BlogCardProps {
+  post: {
+    title: string;
+    tags: string[];
+    date: string;
+    minute: number;
+    imageURL: string;
+  };
+}
+
+const BlogCard: FC<BlogCardProps> = ({ post }) => {
+  return (
+    <div className="max-w-[400px] w-full rounded-lg justify-items-stretch">
+      <div className="flex justify-between ">
+      <h2 className="text-lg font-bold text-x dark:text-white text-gray-800 w-[80%] hover:cursor-pointer line-clamp-2">
+  {post.title}
+</h2>
+
+        <button>
+          <EllipsisVertical className="rounded-xl hover:cursor-pointer"/>
+        </button>
+      </div>
+
+      <div className="flex flex-wrap gap-2 mt-2">
+        {post.tags.map((tag, index) => (
+          <span
+            key={index}
+            className="px-2 py-[1px] text-xs border dark:border-gray-700 rounded-md text-gray-400  hover:cursor-pointer"
+          >
+            #{tag}
+          </span>
+        ))}
+      </div>
+
+
+      <div className="mt-3">
+        <img
+          src={post.imageURL}
+          alt={post.title}
+          className="w-full h-48 object-cover rounded-md  hover:cursor-pointer"
+        />
+      </div>
+      <p className="text-gray-400 text-xs mt-2">
+        {post.date} • {post.minute}m read time
+      </p>
+    </div>
+  );
+};
+
+export default BlogCard;
--- frontend/src/components/user/profile/ProfileFeed.tsx
+++ frontend/src/components/user/profile/ProfileFeed.tsx
@@ -1,32 +1,112 @@
-import { FC, useState } from 'react'
+import { FC, useState } from "react";
+import BlogCard from "../common/BlogCard";
+import { ArrowBigUp,ArrowBigDown } from "lucide-react";
 
-const TAB_OPTIONS = ["Posts", "Replies", "Upvoted"] as const;
+const TAB_OPTIONS = ["Posts", "Archieve", "Saved"] as const;
+const dummyPosts = [
+  {
+    title: "Mastering JavaScript Closures",
+    tags: ["javascript", "functional-programming", "advanced"],
+    date: "Jan 15",
+    minute: 5,
+    imageURL:
+      "https://tse1.mm.bing.net/th?id=OIP.kkJ4tBMv2tT9OqxmUWlQFgHaEK&pid=Api&P=0&h=180",
+    upvotes: 23,
+    downvotes: 4,
+  },
+  {
+    title: "Understanding React's Reconciliation Algorithm",
+    tags: ["react", "performance", "virtual-dom",'sdfdf'],
+    date: "Mar 10",
+    minute: 8,
+    imageURL:
+      "https://tse1.mm.bing.net/th?id=OIP.lyFjc5YkrtksZ1LbAsPuGAHaD4&pid=Api&P=0&h=180",
+    upvotes: 30,
+    downvotes: 1,
+  },
+  {
+    title: "Git Commands That Might Save You One Day",
+    tags: ["productivity", "git", "+1"],
+    date: "Feb 20",
+    minute: 7,
+    imageURL:
+      "https://i.morioh.com/2023/08/18/a0ecc9b5.webp",
+    upvotes: 15,
+    downvotes: 2,
+  },
+  {
+    title: "Building Scalable Node.js Applications",
+    tags: ["nodejs", "backend", "scalability"],
+    date: "Dec 05",
+    minute: 10,
+    imageURL:
+      "https://tse4.mm.bing.net/th?id=OIP.W1ZnAvRNWYSyBsBc0NdwpgHaER&pid=Api&P=0&h=180",
+    upvotes: 18,
+    downvotes: 3,
+  },
+  {
+    title: "A Beginner’s Guide to MongoDB Indexing",
+    tags: ["mongodb", "database", "performance"],
+    date: "Nov 22",
+    minute: 6,
+    imageURL: "https://i.morioh.com/2023/08/18/a0ecc9b5.webp",
+    upvotes: 12,
+    downvotes: 5,
+  },
+];
+
 const ProfileFeed: FC = () => {
-    const [activeTab, setActiveTab] = useState<"Posts" | "Replies" | "Upvoted">("Posts");
+  const [activeTab, setActiveTab] = useState<"Posts" | "Archieve" | "Saved">(
+    "Posts"
+  );
 
-    return (
-        <div className="mt-4 lg:m-0 lg:border flex-1 ">
-            <div className="flex gap-4 justify-around border-b text-gray-600">
-                {TAB_OPTIONS.map((tab) => (
-                    <div className="relative">
-                    <button
-                        key={tab}
-                        className={`px-5 py-2 rounded-lg text-md dark:hover:bg-neutral-500/10 hover:bg-gray-200/30 ${
-                            activeTab === tab ? "font-semibold dark:text-white " : "font-medium"
-                        }`}
-                        onClick={() => setActiveTab(tab)}
-                    >
-                        {tab}
-                    </button>
-                    {activeTab === tab && (
-                        <div className="absolute bottom-0 left-1/2 w-12 -translate-x-1/2 border-b-2 dark:border-white border-black"></div>
-                    )}
-                </div>
-                
-                ))}
+  return (
+    <div className="mt-4 lg:m-0 lg:border flex-1 max-w-[900px]">
+      <div className="flex gap-4 justify-around border-b text-gray-600 sticky top-0 bg-white dark:bg-black">
+        {TAB_OPTIONS.map((tab) => (
+          <div className="relative">
+            <button
+              key={tab}
+              className={`px-5 py-2 rounded-lg text-md dark:hover:bg-neutral-500/10 hover:bg-gray-200/30 ${
+                activeTab === tab
+                  ? "font-semibold dark:text-white "
+                  : "font-medium"
+              }`}
+              onClick={() => setActiveTab(tab)}
+            >
+              {tab}
+            </button>
+            {activeTab === tab && (
+              <div className="absolute bottom-0 left-1/2 w-12 -translate-x-1/2 border-b-2 dark:border-white border-black"></div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-wrap justify-center  p-4 h-[85vh] lg:overflow-y-auto" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+        {dummyPosts.length>0?(
+          dummyPosts.map((post, index) => (
+            <div
+              key={index}
+              className="p-2 m-1 border-2 rounded-lg max-w-[300px] flex flex-col justify-between"
+            >   
+              <BlogCard post={post} />
+              <div className="flex ml-auto mt-3 px-2 text-gray-600 dark:text-gray-400 bg-gray-100 dark:bg-gray-800 w-fit rounded-md ">
+                <button className="flex items-center gap-1 text-sm  hover:cursor-pointer">
+                  <ArrowBigUp strokeWidth={1}/> {post.upvotes}
+                </button>
+                <button className="flex items-center gap-1 text-sm  hover:cursor-pointer ml-2">
+                  <ArrowBigDown strokeWidth={1}/> 
+                </button>
+              </div>
             </div>
-        </div>
-    );
+          ))
+        ):(
+          <p className="text-gray-600 text-center text-2xl font-medium  col-span-3 mt-14">No posts available !</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ProfileFeed;
