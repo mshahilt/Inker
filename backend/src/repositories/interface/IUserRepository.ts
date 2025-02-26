03e5cf2017437af320cf4fff7d2c8fbbba2c9828
SHAMNAD-S404 <shamnadsakeer306@gmail.com>
2025-02-26 16:56:36 +0530

    feat: added uuid implemenation

--- backend/src/repositories/interface/IUserRepository.ts
+++ backend/src/repositories/interface/IUserRepository.ts
@@ -1,4 +1,4 @@
-import {IUserModel} from "@/src/models/implementation/user.model";
+import { IUserModel } from "@/models/implementation/user.model";
 
 export interface IUserRepository {
     create(user: IUserModel): Promise<IUserModel>;
