e74bb7d6f99720f462fe67af78860d2c1847d3e1
SHAMNAD-S404 <shamnadsakeer306@gmail.com>
2025-02-26 18:41:11 +0530

    refact : auth controller

--- backend/src/utils/generate-uniq-username.ts
+++ backend/src/utils/generate-uniq-username.ts
@@ -3,17 +3,13 @@ import { UserRepository } from "@/repositories/implementation/user.repository";
 const userRepository = new UserRepository();
 
 export const generateUniqueUsername = async (name: string) => {
-  try {
-    const baseUsername = name.trim().toLocaleLowerCase().replace(/\s+/g, "_");
-    let username = baseUsername;
+  const baseUsername = name.trim().toLocaleLowerCase().replace(/\s+/g, "_");
+  let username = baseUsername;
 
-    while (await userRepository.findByUsername(username)) {
-      let counter = Math.floor(100 + Math.random() * 900);
-      username = `${baseUsername}${counter}`;
-    }
-
-    return username;
-  } catch (error) {
-    throw error;
+  while (await userRepository.findByUsername(username)) {
+    const counter = Math.floor(100 + Math.random() * 900);
+    username = `${baseUsername}${counter}`;
   }
+
+  return username;
 };
