46a7306e8b8c149642e518ac1a57f4b2c2e0cc98
Udhaykrishn <krishnauday320@gmail.com>
2025-03-09 21:19:34 +0530

    feat: implement verify user email method

--- backend/src/services/interface/IUserService.ts
+++ backend/src/services/interface/IUserService.ts
@@ -1,3 +1,5 @@
+import { IUserModel } from "@/models/implementation/user.model";
+
 export interface IUserService {
-    verifyEmail(id: string, email: string): Promise<void>
+    verifyEmail(id: string, email: string): Promise<IUserModel | null>
 }
