498171a5d9661cc60e1de87171a863c7fa44345a
Udhaykrishn <krishnauday320@gmail.com>
2025-03-11 12:18:12 +0530

    feat(change-email): implement change user email service

--- backend/src/services/implementation/profile.service.ts
+++ backend/src/services/implementation/profile.service.ts
@@ -1,10 +1,9 @@
 import { IUserRepository } from "@/repositories/interface/IUserRepository";
 import { IProfileService } from "../interface/IProfileService";
-import { createHttpError } from "@/utils/http-error.util";
+import { createHttpError, checkEmailExistence } from "@/utils";
 import { HttpStatus } from "@/constants/status.constant";
 import { HttpResponse } from "@/constants/response-message.constant";
 import { IUserModel } from "@/models/implementation/user.model";
-
 
 //!   Implementation for Profile Service
 export class ProfileService implements IProfileService {
@@ -34,19 +33,43 @@ export class ProfileService implements IProfileService {
     return user ? user.username : undefined;
   }
 
-  async updateProfile(id:string,updateData:Partial<IUserModel>): Promise<IUserModel>{
+  async updateProfile(id: string, updateData: Partial<IUserModel>): Promise<IUserModel> {
     const isExist = await this._userRepository.findUserById(id);
 
-    if(!isExist){
+    if (!isExist) {
       throw createHttpError(HttpStatus.NOT_FOUND, HttpResponse.USER_NOT_FOUND);
     }
 
-    const updatedData = await this._userRepository.updateUserProfile(id,updateData) 
-    
-    if(!updatedData){
-      throw createHttpError(HttpStatus.NOT_FOUND,HttpResponse.USER_NOT_FOUND)
+    const updatedData = await this._userRepository.updateUserProfile(id, updateData)
+
+    if (!updatedData) {
+      throw createHttpError(HttpStatus.NOT_FOUND, HttpResponse.USER_NOT_FOUND)
     }
     return updatedData
+  }
+
+  async updateEmail(id: string, email: string): Promise<IUserModel> {
+    const existingEmail = await this._userRepository.findByEmail(email);
+
+
+    if (existingEmail) {
+      throw createHttpError(HttpStatus.NOT_FOUND, HttpResponse.USER_EXIST)
+    }
+
+    const isEmailReal = await checkEmailExistence(email)
+
+    if (!isEmailReal) {
+      throw createHttpError(HttpStatus.BAD_REQUEST, HttpResponse.INVALID_EMAIL)
+    }
+
+    // need otp verification before updating email
+    const updateEmail = await this._userRepository.updateEmail(id, email)
+
+    if (!updateEmail) {
+      throw createHttpError(HttpStatus.NOT_FOUND, HttpResponse.USER_NOT_FOUND)
+    }
+
+    return updateEmail;
   }
 
 }
