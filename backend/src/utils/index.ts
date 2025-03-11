8e001841b6024e1f2e2237d5b2613fffdf4143d4
Udhaykrishn <krishnauday320@gmail.com>
2025-03-11 12:26:17 +0530

    feat: re-export all utils functions

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,8 +1,9 @@
-export * from "./validate-env.util"
-export * from "./send-email.util"
-export * from "./jwt.util"
-export * from "./http-error.util"
-export * from "./generate-otp.util"
-export * from "./bcrypt.util"
-export * from "./email-verification.utils"
-export * from "./convert-object-id.utils"
+export { validateEnv } from "./validate-env.util";
+export { sendOtpEmail, sendResetPasswordEmail } from "./send-email.util";
+export { generateAccessToken, generateRefreshToken, verifyAccessToken, verifyRefreshToken } from "./jwt.util";
+export { createHttpError, HttpError } from "./http-error.util";
+export { generateOTP } from "./generate-otp.util";
+export { hashPassword, comparePassword } from "./bcrypt.util";
+export { checkEmailExistence } from "./email-verification.utils";
+export { toObjectId } from "./convert-object-id.utils";
+export { generateUniqueUsername } from "./generate-uniq-username"
