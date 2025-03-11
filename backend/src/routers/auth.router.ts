b71ba3ac997f3c8854786050529f3d0643bac3ca
Udhaykrishn <krishnauday320@gmail.com>
2025-03-11 12:02:05 +0530

    feat: import all schema functions in one place

--- backend/src/routers/auth.router.ts
+++ backend/src/routers/auth.router.ts
@@ -3,11 +3,14 @@ import { AuthController } from "../controllers/implementation/auth.controller";
 import { AuthService } from "../services/implementation/auth.service";
 import { UserRepository } from "../repositories/implementation/user.repository";
 import { validate } from "@/middlewares/validate.middleware";
-import { signupSchema } from "@/schema/signup-schema";
-import signinSchema from "@/schema/signin.schema";
-import verifyOtpSchema from "@/schema/verify-otp.schema";
-import verifyEmailScheam from "@/schema/forgot-pass.schema";
-import resetPasswordSchema from "@/schema/reset-pass.schema";
+import {
+  signupSchema,
+  signinSchema,
+  verifyEmailSchema,
+  verifyOtpSchema,
+  resetPasswordSchema
+} from "@/schema";
+
 
 const authRouter = Router();
 
@@ -34,7 +37,7 @@ authRouter.post(
 
 authRouter.post(
   "/forgot-password",
-  validate(verifyEmailScheam),
+  validate(verifyEmailSchema),
   authController.forgotPassword.bind(authController)
 );
 
