754138311f7f36e6221a35b020bb18a6f4425c3e
AJMALAJU3 <ajmalaju3@users.noreply.github.com>
2025-03-13 16:13:40 +0530

    remove unused imported reacts

--- frontend/src/components/user/editProfile/UpdateProfile.tsx
+++ frontend/src/components/user/editProfile/UpdateProfile.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from '@/components/ui/textarea'
