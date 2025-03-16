c932896703cf49bf0d0fffb613df96774b45981d
SujithVSuresh <sujitheternal13@gmail.com>
2025-03-16 15:17:15 +0530

    feat: add create  blog endpoint to blog service

---
+++ frontend/src/services/blogServices.ts
@@ -0,0 +1,28 @@
+import { axiosInstance } from "@/config/axios";
+import { toast } from "sonner";
+import { AxiosError } from "axios";
+
+interface BlogData {
+  title: string;
+  author: string;
+  authorId: string;
+  tags: string[];
+  thumbnail: {
+    type: string;
+    url: string;
+  };
+  content: string;
+}
+
+export const blogService = {
+  createBlogService: async (data: BlogData): Promise<{ status: number; message: string }> => {
+    try {
+      const response = await axiosInstance.post<{status: number; message: string;}>("/api/blog", data);
+      return response.data
+    } catch (error: unknown) {
+      const err = error as AxiosError<{ error: string }>;
+      toast.error(err.response?.data?.error || "Login failed. Please try again.");
+      throw new Error(err.response?.data?.error || "Login failed.");
+    }
+  },
+};
