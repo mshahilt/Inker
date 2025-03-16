beefb9af2f7a3401aca916e52a58ba91f06e15c5
SujithVSuresh <sujitheternal13@gmail.com>
2025-03-16 15:34:33 +0530

    feat: add blog deletion api endpoint to blog service

--- frontend/src/services/blogServices.ts
+++ frontend/src/services/blogServices.ts
@@ -25,4 +25,20 @@ export const blogService = {
       throw new Error(err.response?.data?.error || "Blog creation failed.");
     }
   },
+
+  deleteBlogService: async ({blogId, authorId}: {blogId: string; authorId: string}): Promise<{ status: number; message: string }> => {
+    try {
+      const response = await axiosInstance.delete<{status: number; message: string;}>("/api/blog", {
+        params: {
+            blogId,
+            authorId
+        }
+      });
+      return response.data
+    } catch (error: unknown) {
+      const err = error as AxiosError<{ error: string }>;
+      toast.error(err.response?.data?.error || "Blog deletion failed. Please try again.");
+      throw new Error(err.response?.data?.error || "Blog deletion failed.");
+    }
+  },
 };
